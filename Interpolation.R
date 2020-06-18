e41716da4ecdce788571983ce4f9757c4e53d651
Carsten <crstn@users.noreply.github.com>
2020-06-18 07:20:33 +0200

    Initial interpolations added

--- Interpolation.R
+++ Interpolation.R
@@ -1,5 +1,9 @@
 # the sp library has all the interpolation stuff in R
 library(sp)
+library(sf)
+library(dismo)
+library(deldir)
+library(gstat)
 
 # set the working directory in R Studio:
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -14,13 +18,14 @@ stations <- stations[c('Name', 'east', 'north')]
 stations #check
 
 # load the daily precipitation data
-precip <- read.table("niederschlagsdaten/nieder_tageswerte.txt", 
+precip <- read.table("Niederschlagsdaten/nieder_tageswerte.txt", 
                      header = TRUE, sep = ";", dec = ".", fileEncoding="latin1")
 precip # you get the idea...
 
-# we will only look at June 14, 2020 - that day has a nice distribution of different 
+# we will only look at June 5, 2020 - that day has a nice distribution of different 
 # values across NRW
-precip <- precip[(precip$Datum == "2020-06-14"),]
+datum = "2020-06-05"
+precip <- precip[(precip$Datum == datum),]
 precip 
 
 # only keep the value and the station name
@@ -32,6 +37,58 @@ precip <- merge(stations, precip, by="Name")
 precip
 
 # write that out, so we can look it in a GIS etc.
-write.csv(precip, "June14.csv") 
+write.csv(precip, paste(datum, ".csv", sep="")) 
+
+# Ready to go. Let's turn the data into a SpatialPointsDataFrame
+preciPoints <- SpatialPoints(precip[,2:3], proj4string=CRS("+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")) #UTM 32N
+preciPoints <- SpatialPointsDataFrame(preciPoints, precip[,c(1,4)])
+
+# check the maximum value
+max(preciPoints$Tagessumme)
+
+# read NRW shape file to plot the state boundary
+NRW.sf <- st_read("NRW/NRW.shp")
+NRW.sp <- as(NRW.sf, "Spatial")
+NRW.pols <- list("sp.polygons", NRW.sp, fill = "lightgray")
+
+spplot(preciPoints, 'Tagessumme',sp.layout=NRW.pols, 
+       pch=20, cex=1, cuts=15, colorkey=TRUE)
+
+
+# nearest neighbor interpolation
+
+# just to demonstrate, make a Delaunay triangulation:
+vtess=deldir(preciPoints@coords[,1],preciPoints@coords[,2])
+plot(vtess, wlines='triang')
+plot(vtess, wlines='both')
+
+
+# create voronoi polygons
+voro <- voronoi(preciPoints)
+plot(voro)
+
+# clip to NRW
+voro.nrw <- intersect(voro, aggregate(NRW.sp))
+spplot(voro.nrw, 'Tagessumme', main="Nearest neighbor interpolation")
+
+
+# IDW
+# blank raster
+r <- raster(NRW, res=500)
+
+gs <- gstat(formula=Tagessumme~1, locations=preciPoints, nmax=7, set = list(idp = 1))
+idw <- interpolate(r, gs)
+## [inverse distance weighted interpolation]
+idwr <- mask(idw, NRW)
+spplot(idwr)
+
+
+# define root mean square error
+RMSE <- function(observed, predicted) {
+  sqrt(mean((predicted - observed)^2, na.rm=TRUE))
+}
+
+
+
 
 
